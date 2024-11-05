ec65744c6b865562eaee49853b9167d9bbaa6817
Stacie Spahr <stacie.spahr@plu.edu>
2024-11-05 12:21:11 -0800

    feat: React setup

---
+++ bigfoot/src/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import { Typography } from '@mui/material';
+
+import HomePage from './routes/HomePage'; // Import your HomePage component
+
+function App() {
+  return (
+    <div>
+      <Typography variant="h4">Home Page</Typography>
+    </div>
+  );
+}
+
+export default App;
